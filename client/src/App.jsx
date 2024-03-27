e78480ef1800b8f1aa59c9fb302a2ed0986d3f0f
Cascade8598 <tmdft360@gmail.com>
2024-03-27 11:00:05 +0000

    feature: add style to Link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import Navigation from './Navigation';
+import { Container } from '@mui/material';
 import './App.css';
 function App() {
 
   return (
-    <>
+    <Container className='navigation-container-wrapper'>
      <Navigation/>
-    </>
+    </Container>
   );
 }
 
