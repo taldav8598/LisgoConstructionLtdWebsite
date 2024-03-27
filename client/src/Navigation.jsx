955d3fd5847ea3a72db6b64c5c5368718c2b5e9f
Cascade8598 <tmdft360@gmail.com>
2024-03-27 10:00:12 +0000

    feature: add empty hyperlink and style to navigation link

--- client/src/Navigation.jsx
+++ client/src/Navigation.jsx
@@ -1,13 +1,14 @@
 import { Container, Link } from '@mui/material';
+import './Navigation.css';
 
 export default function Navigation() {
     return (
-        <Container>
-            <Link>Home</Link>
-            <Link>About us</Link>
-            <Link>Services</Link>
-            <Link>Gallery</Link>
-            <Link>Contacts</Link>
+        <Container className='navigation-container'>
+            <Link href='#' className='navigation-link' underline='hover'>Home</Link>
+            <Link href='#' className='navigation-link' underline='hover'>About us</Link>
+            <Link href='#' className='navigation-link' underline='hover'>Services</Link>
+            <Link href='#' className='navigation-link' underline='hover'>Gallery</Link>
+            <Link href='#' className='navigation-link' underline='hover'>Contacts</Link>
         </Container>
     );
 }
