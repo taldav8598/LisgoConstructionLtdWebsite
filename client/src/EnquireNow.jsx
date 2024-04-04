0abd6c05f197256e6a44b2e8af74db6dcaa00eaf
Cascade8598 <tmdft360@gmail.com>
2024-04-04 10:08:33 +0100

    feature: add logic for appointment booking in EnquireNow component

--- client/src/EnquireNow.jsx
+++ client/src/EnquireNow.jsx
@@ -10,7 +10,8 @@ const formattedMinDate = date.toISOString();
         axios
         .get(`https://www.googleapis.com/calendar/v3/calendars/24dd8a69a809ae107ee7047d82890a76acb6944dfc635d195d7fe5a313e8c158@group.calendar.google.com/events?key=AIzaSyDENYFe9hlrFL_zp_d50TS520ujLU0Dqg0&timeMin=${formattedMinDate}`)
         .then(({data}) => console.log('Google clanedar response', data));
-    }, [])
+    });
+    console.log(formattedMinDate);
     return (
         <section id="test">
             <h1>Enquire now</h1>
