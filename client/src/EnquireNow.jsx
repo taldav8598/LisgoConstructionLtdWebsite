965c1498d0a0ba235704483b3cdfdec26df854a8
JOSEPH-NEWTON-CIC <JOSEPH-NEWTON-CIC@ibm.com>
2024-04-05 12:49:40 +0100

    added proposed solution

--- client/src/EnquireNow.jsx
+++ client/src/EnquireNow.jsx
@@ -18,8 +18,8 @@ export default function EnquireNow() {
 
     const dateFormatting = async () => {
         //uncommented for git push
-        // const date = new Date();
-        // const formattedMinDate = date.toISOString();
+        const date = new Date();
+        const formattedMinDate = date.toISOString();
 
         
         // const timeStampDate = Date.parse(formattedMinDate);
@@ -32,7 +32,7 @@ export default function EnquireNow() {
             const { items } = data;
             // console.log('items', items);
             // dates: ["2024-04-04"], dateTimes: {"2024-04-04-16:30"}
-            let dateObjectList = { dates: [], dateTimes: []};
+            let dateObjectList = { dates: [], dateTimes: {start: [], end: []}};
 
             items.forEach(item => {
                 // start:[{date: 2024-04-04}, {time: 16:30:00}]
@@ -41,14 +41,16 @@ export default function EnquireNow() {
                 if (item.start.dateTime) {
                     // console.log("start Datetime", item.start.dateTime);
                     const date = new Date(item.start.dateTime)
+                    const endDate = new Date(item.end.dateTime)
                     const localeString = date.toLocaleTimeString('en-UK');
+                    const localeStringEnd = endDate.toLocaleTimeString('en-UK');
                     console.log('localeString', localeString);
                     let shortenedDate = `${date.getFullYear()}-${(date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1)}-${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}`;
-                    let formattedDateTime = item.start.dateTime.replace(/:[0-9][0-9]\+[0-9][0-9]:00/g, '').replace(/T/, '-');
+                    // let formattedDateTime = item.start.dateTime.replace(/:[0-9][0-9]\+[0-9][0-9]:00/g, '').replace(/T/, '-');
                     // console.log("shortenedDate", shortenedDate);
-                    console.log("item", item.start.dateTime.split("-").slice(-1));
                     dateObjectList.dates.push(shortenedDate);
-                    dateObjectList.dateTimes.push(formattedDateTime);
+                    dateObjectList.dateTimes.start.push(Number(localeString.split(':').join('')));
+                    dateObjectList.dateTimes.end.push(Number(localeStringEnd.split(':').join('')));
                 }
             });
             // console.log("dateObjectList", dateObjectList);
@@ -90,23 +92,35 @@ export default function EnquireNow() {
     }
 
     const disabledTimes = (date) => {
-        const formattedDate = `${date['$y']}-${ date['$M'] + 1 < 10 ? "0" + (date['$M'] + 1) : (date['$M'] + 1)}-${date['$D'] < 10 ? "0" + date['$D'] : date['$D']}`;
+
+        console.log(date, ' ---------> date from calendar')
+        console.log(dateLists.dateObjectList?.dateTimes.start, dateLists.dateObjectList?.dateTimes.end, dateLists.dateObjectList?.dates,  '---------> datessssssss')
+
+
+        //17
+        //10  
+
+        // 7 loop over starting 12 [{12: true}, {13: true}, {14: false}, {15: true}, {14: false}, {15: false}, {14: true}, {17: 00}]
+
+
+        // .find to find the date first and then use the index to compare the start time arr and the end time arr. If the times are inbetween these values on the set date then disable them
+        // const formattedDate = `${date['$y']}-${ date['$M'] + 1 < 10 ? "0" + (date['$M'] + 1) : (date['$M'] + 1)}-${date['$D'] < 10 ? "0" + date['$D'] : date['$D']}`;
         // 12:05
-        const formattedTime = `${date['$H'] < 10 ? 0 + date['$H'] : date['$H']}:${date['$m'] < 10 ? '0' + date['$m'] : date['$m']}`;
-        const formattedDateTime = formattedDate + "-" + formattedTime;
+        // const formattedTime = `${date['$H'] < 10 ? 0 + date['$H'] : date['$H']}:${date['$m'] < 10 ? '0' + date['$m'] : date['$m']}`;
+        // const formattedDateTime = formattedDate + "-" + formattedTime;
         // console.log("disabledTimesDate", date['$H'] >= 9 && date['$H'] <= 17)
         // console.log(dateLists.dateObjectList?.dateTimes[0].slice(0, 10), dateLists.dateObjectList?.dateTimes[0].slice(11, ))
         // console.log(formattedDateTime);
         // console.log(dateLists.dateObjectList?.dateTimes);
-        const formattedDateTimeIsPresent = dateLists.dateObjectList?.dateTimes.includes(formattedDateTime);
-        console.log('formattedDateTimeIsPresent', formattedDateTimeIsPresent)
-        if (formattedDateTimeIsPresent) {
-            console.log(formattedDateTime);
-            let reservedHour = date['$H'];
-            console.log('reservedHour', reservedHour)
-            // let reservedMinutes = date['$m'];
-            return !(date['$H'] >= 9 && date['$H'] <= 17) && date['$H'] === reservedHour;
-        }
+        // const formattedDateTimeIsPresent = dateLists.dateObjectList?.dateTimes.includes(formattedDateTime);
+        // console.log('formattedDateTimeIsPresent', formattedDateTimeIsPresent)
+        // if (formattedDateTimeIsPresent) {
+        //     console.log(formattedDateTime);
+        //     let reservedHour = date['$H'];
+        //     console.log('reservedHour', reservedHour)
+        //     // let reservedMinutes = date['$m'];
+        //     return !(date['$H'] >= 9 && date['$H'] <= 17) && date['$H'] === reservedHour;
+        // }
         // Disable the hours which are not between 9am and 5pm
         return !(date['$H'] >= 9 && date['$H'] <= 17);
     }
