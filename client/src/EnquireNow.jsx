5c3519cbe4559c67855e320b53063d24375758a4
JOSEPH-NEWTON-CIC <JOSEPH-NEWTON-CIC@ibm.com>
2024-04-10 19:42:22 +0100

    got email working

--- client/src/EnquireNow.jsx
+++ client/src/EnquireNow.jsx
@@ -7,6 +7,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
 import { useEffect, useState } from 'react';
+import emailjs from 'emailjs-com';
 import axios from 'axios';
 import './EnquireNow.css'
 // import { ThemeProvider } from '@emotion/react';
@@ -73,21 +74,23 @@ const theme = createTheme({
   });
 
 export default function EnquireNow() {
+    const today = dayjs();
     const [dateLists, setDateLists] = useState({});
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [message, setMessage] = useState('');
-    const [dateInput, setDateInput] = useState('');
+    const [dateInput, setDateInput] = useState(new Date(today).toLocaleString());
+    const [showSuccessMessage, setShowSuccessMessage] = useState('');
 
 // RFC 3339 format
 
-    const today = dayjs();
+   
 
     const dateFormatting = async () => {
         //uncommented for git push
-        const date = new Date();
-        const formattedMinDate = date.toISOString();
+        // const date = new Date();
+        // const formattedMinDate = date.toISOString();
 
 
         // const timeStampDate = Date.parse(formattedMinDate);
@@ -96,14 +99,8 @@ export default function EnquireNow() {
         await axios
         .get(``)
         .then(({ data }) => {
-            // console.log("data", data);
             const { items } = data;
-            // {
-            //     dateIndex = 0
-            //     dates: [ '2024-04-08' ],
-            //     dateTimeIndex = 0 in start and end for the 8th April 2024 or 2024-04-08 
-            //     dateTime: { start: [ [ '090000', '091500', '093000', '094500' ] ], end: [ [ '091500', '093000', '094500', '100000' ] ] }
-            // }
+        
             let dateObjectList = { 
                 dates: [],
                 dateTimes: { start: [], end: [] } 
@@ -122,8 +119,8 @@ export default function EnquireNow() {
 
                     let shortenedDate = `${date.getFullYear()}-${(date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1)}-${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}`;
                     
-                    const formattedLocaleString = Number(localeString.split(':').join(''));
-                    console.log(!dateObjectList.dates.includes(formattedLocaleString) ? dateObjectList.dates : false);
+                    // const formattedLocaleString = Number(localeString.split(':').join(''));
+                    // console.log(!dateObjectList.dates.includes(formattedLocaleString) ? dateObjectList.dates : false);
 
                     dateObjectList.dates.push(Number(shortenedDate.replaceAll('-', '')));
                     
@@ -131,7 +128,6 @@ export default function EnquireNow() {
                     dateObjectList.dateTimes.end.push(Number(localeStringEnd.split(':').join('')));
                 }
             });
-            console.log(dateObjectList.dates, dateObjectList.dateTimes)
             return setDateLists(prevState => ({...prevState, dateObjectList}));
         });
     } 
@@ -154,9 +150,9 @@ export default function EnquireNow() {
         let hour = date['$H'];
         let minute = date['$m'];
 
-        let formattedMinute = Number(minute + '00')
+        // let formattedMinute = Number(minute + '00')
 
-        console.log(formattedMinute < Number(dateLists.dateObjectList.dateTimes.start[0].toString().slice(2, dateLists.dateObjectList.dateTimes.start[0].length)));
+        // console.log(formattedMinute < Number(dateLists.dateObjectList.dateTimes.start[0].toString().slice(2, dateLists.dateObjectList.dateTimes.start[0].length)));
         
         const formattedDate = `${date['$y']}-${ date['$M'] + 1 < 10 ? "0" + (date['$M'] + 1) : (date['$M'] + 1)}-${date['$D'] < 10 ? "0" + date['$D'] : date['$D']}`;
 
@@ -196,13 +192,27 @@ export default function EnquireNow() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const { target } = e;
-        console.log( target.value );
+        document.getElementById('enquiry-description-textfield').value =  `Details: Name: ${name} \n Email: ${email} \n Phone number: ${phoneNumber} \n Date of appointment: ${dateInput} \n Enquiry info: ${message}`;
+        const element = document.getElementById('enquiry-description-textfield');
+        element.style.display = 'none';
+        
+    emailjs.sendForm('service_rzgfdla', 'template_7mhh8i9', e.target, 'iejlvPp_cY_iY6NNE')
+      .then((result) => {
+        e.target.reset();
+        element.style.display = 'block';
+        setShowSuccessMessage(true)
+        setTimeout(() => {
+            setShowSuccessMessage(false)
+             // Or any other display value you want
+        }, 5000);
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
     }
 
     const handleDateChange = (date) => {
         const date2 = new Date(date['$d'])
-        console.log("new console.log ->", date2.toLocaleString());
         return setDateInput(date2.toLocaleString());
     }
 
@@ -220,27 +230,27 @@ export default function EnquireNow() {
                 <InputLabel htmlFor="name-input">Name</InputLabel>
                 <InputLabel htmlFor="name-input">*required field</InputLabel>
                 </div>
-                <Input id='name-input' variant='outlined' onChange={({ target }) => setName(target.value)} value={name}></Input>
-                {name}
+                <Input name="from_name" id='name-input' variant='outlined' onChange={({ target }) => setName(target.value)} value={name}></Input>
                 <div className='label-wrapper'>
                 <InputLabel htmlFor="email-input">Email</InputLabel>
                 <InputLabel htmlFor="name-input">*required field</InputLabel>
                 </div>
-                <Input id='email-input' variant='outlined' onChange={({ target }) => (console.log('------> target.value',target.value), setEmail(target.value))}></Input>
+                <Input name="email" id='email-input' variant='outlined' value={email} onChange={({ target }) => (setEmail(target.value))}></Input>
                 <InputLabel htmlFor="phone-number-input">Phone number</InputLabel>
-                <Input id='phone-number-input' variant='outlined' onChange={({ target }) => (console.log('------> target.value',target.value), setPhoneNumber(target.value))}></Input>
+                <Input name="phone_number" id='phone-number-input' variant='outlined' value={phoneNumber} onChange={({ target }) => (setPhoneNumber(target.value))}></Input>
                 <InputLabel htmlFor="enquiry-description-textfield">Message</InputLabel>
                 <TextField
                 id="enquiry-description-textfield"
+                name="message"
                 multiline
                 rows={4}
                 fullWidth={true}
                 placeholder="Please provide a description of your enquiry"
-                onChange={({ target }) => (console.log('------> target.value',target.value), setMessage(target.value))}
+                onChange={({ target }) => (setMessage(target.value))}
                 />
                 {/* <InputLabel htmlFor="choose-file-picker">Choose a file</InputLabel>
                 <Button id="choose-file-button">Choose a file</Button> */}
-                <InputLabel htmlFor="name-input">Choose a time for your appointment</InputLabel>
+                <InputLabel htmlFor="name-input">Choose a date or time of when you want the work to start or when you would like an appointment</InputLabel>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DemoContainer components={['DateTimePicker']}>
                         <DemoItem>
@@ -254,13 +264,17 @@ export default function EnquireNow() {
                             disablePast={true}
                             timeSteps={{minutes: 15}}
                             onChange={handleDateChange}
+                            name="start_date"
                             />
                         </DemoItem>
                 </DemoContainer>
             </LocalizationProvider>
-                            <h1>{ dateInput }</h1>
             <Button id="submit-button" type='submit' fullWidth={true} disabled={ !name || !email } variant='contain'>submit</Button>
+
             </form>
+            {showSuccessMessage && (<p className='success-message'>Message sent successfully</p>)}
+
+
             </Container>
             </ThemeProvider>
 
