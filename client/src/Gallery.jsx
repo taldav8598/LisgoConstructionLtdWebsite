1bb656117dd0cfef71493eea07c5b6b78f6d6d4d
Cascade8598 <tmdft360@gmail.com>
2024-04-10 10:46:46 +0100

    feature: add gallery grid layout

--- client/src/Gallery.jsx
+++ client/src/Gallery.jsx
@@ -6,15 +6,15 @@ export default function Gallery() {
     return (
         <section className='gallery-section'>
         <div className='gallery-container'>
-            <div className="imageOne"></div>
+            <div className="imageOne">1</div>
             <Typography variant="h3">Gallery</Typography>
-            <div className="imageTwo"></div>
-            <div className="imageThree"></div>
-            <div className="imageFour"></div>
-            <div className="imageFive"></div>
-            <div className="imageSix"></div>
-            <div className="imageSeven"></div>
-            <div className="imageEight"></div>
+            <div className="imageTwo">2</div>
+            <div className="imageThree">3</div>
+            <div className="imageFour">4</div>
+            <div className="imageFive">5</div>
+            <div className="imageSix">6</div>
+            <div className="imageSeven">7</div>
+            <div className="imageEight">8</div>
         </div>
         </section>
     );
