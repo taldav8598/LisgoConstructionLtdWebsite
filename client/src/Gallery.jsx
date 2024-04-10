f17827f59d2938a0e7e340f07029c9c513167f48
Cascade8598 <tmdft360@gmail.com>
2024-04-10 09:44:22 +0100

    feature: add image containers

---
+++ client/src/Gallery.jsx
@@ -0,0 +1,21 @@
+import { Typography } from '@mui/material';
+import './Gallery.css';
+
+export default function Gallery() {
+
+    return (
+        <section className='gallery-section'>
+        <div className='gallery-container'>
+            <div className="imageOne"></div>
+            <Typography variant="h3">Gallery</Typography>
+            <div className="imageTwo"></div>
+            <div className="imageThree"></div>
+            <div className="imageFour"></div>
+            <div className="imageFive"></div>
+            <div className="imageSix"></div>
+            <div className="imageSeven"></div>
+            <div className="imageEight"></div>
+        </div>
+        </section>
+    );
+}
