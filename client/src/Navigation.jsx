1b6b941eba8abae187906d5c0e71bd9b85d6935d
Cascade8598 <tmdft360@gmail.com>
2024-10-28 12:36:11 +0000

    feature: adds contact us link to navigation

--- client/src/Navigation.jsx
+++ client/src/Navigation.jsx
@@ -1,22 +1,32 @@
-import { Container, Link } from '@mui/material';
-import './Navigation.css';
-import LisgoLogo from './assets/LisgoLogo.jpg';
+import { Container, Link } from "@mui/material";
+import "./Navigation.css";
+import LisgoLogo from "./assets/LisgoLogo.jpg";
 
 export default function Navigation() {
-    return (
-        <Container className='navigation-container'>
-            <div className='LisgoLogoContainer'>
-            <img
-            className='LisgoLogo'
-            src={LisgoLogo}
-            />
-            <p className='Lisgo-Logo-Heading'>Lisgo Construction Ltd.</p>
-            </div>
-            <Link href='#home' className='navigation-link' underline='hover'>Home</Link>
-            <Link href='#about-us' className='navigation-link' underline='hover'>About us</Link>
-            <Link href='#enquire' className='navigation-link' underline='hover'>Enquire now</Link>
-            <Link href='#services' className='navigation-link' underline='hover'>Services</Link>
-            <Link href='#gallery' className='navigation-link' underline='hover'>Gallery</Link>
-        </Container>
-    );
+  return (
+    <Container className="navigation-container">
+      <div className="LisgoLogoContainer">
+        <img className="LisgoLogo" src={LisgoLogo} />
+        <p className="Lisgo-Logo-Heading">Lisgo Construction Ltd.</p>
+      </div>
+      <Link href="#home" className="navigation-link" underline="hover">
+        Home
+      </Link>
+      <Link href="#about-us" className="navigation-link" underline="hover">
+        About us
+      </Link>
+      <Link href="#enquire" className="navigation-link" underline="hover">
+        Enquire now
+      </Link>
+      <Link href="#services" className="navigation-link" underline="hover">
+        Services
+      </Link>
+      <Link href="#gallery" className="navigation-link" underline="hover">
+        Gallery
+      </Link>
+      <Link href="#contact-us" className="navigation-link" underline="hover">
+        Contact us
+      </Link>
+    </Container>
+  );
 }
