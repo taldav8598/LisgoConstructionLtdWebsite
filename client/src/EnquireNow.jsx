bf9f5fe5b2fb763a7482bf40dfd7d12b396f61d0
Cascade8598 <tmdft360@gmail.com>
2024-10-30 19:01:03 +0000

    fix: to dateTimePicker display

--- client/src/EnquireNow.jsx
+++ client/src/EnquireNow.jsx
@@ -114,7 +114,6 @@ export default function EnquireNow() {
   const [validPostcode, setValidPostcode] = useState(true);
 
   // RFC 3339 format
-  console.log("dateLists", dateLists);
 
   const days = {
     0: "Sunday",
@@ -128,44 +127,49 @@ export default function EnquireNow() {
   const dateFormatting = async () => {
     const date = new Date();
     const formattedMinDate = date.toISOString();
-    await axios.get(`${apiUrl}${formattedMinDate}`).then(({ data }) => {
-      const { items } = data;
+    await axios
+      .get(`${apiUrl}${formattedMinDate}`)
+      .then(({ data }) => {
+        const { items } = data;
 
-      let dateObjectList = {
-        dates: [],
-        dateTimes: { start: [], end: [] },
-      };
+        let dateObjectList = {
+          dates: [],
+          dateTimes: { start: [], end: [] },
+        };
 
-      items.forEach((item) => {
-        if (item.start.dateTime) {
-          const date = new Date(item.start.dateTime);
-          const endDate = new Date(item.end.dateTime);
+        items.forEach((item) => {
+          if (item.start.dateTime) {
+            const date = new Date(item.start.dateTime);
+            const endDate = new Date(item.end.dateTime);
 
-          const localeString = date.toLocaleTimeString("en-UK");
-          const localeStringEnd = endDate.toLocaleTimeString("en-UK");
+            const localeString = date.toLocaleTimeString("en-UK");
+            const localeStringEnd = endDate.toLocaleTimeString("en-UK");
 
-          let shortenedDate = `${date.getFullYear()}-${
-            date.getMonth() + 1 < 10
-              ? "0" + (date.getMonth() + 1)
-              : date.getMonth() + 1
-          }-${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}`;
+            let shortenedDate = `${date.getFullYear()}-${
+              date.getMonth() + 1 < 10
+                ? "0" + (date.getMonth() + 1)
+                : date.getMonth() + 1
+            }-${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}`;
 
-          dateObjectList.dates.push(Number(shortenedDate.replaceAll("-", "")));
+            dateObjectList.dates.push(
+              Number(shortenedDate.replaceAll("-", ""))
+            );
 
-          dateObjectList.dateTimes.start.push(
-            Number(localeString.split(":").join(""))
-          );
-          dateObjectList.dateTimes.end.push(
-            Number(localeStringEnd.split(":").join(""))
-          );
-        }
-      });
-      return setDateLists((prevState) => ({ ...prevState, dateObjectList }));
-    });
+            dateObjectList.dateTimes.start.push(
+              Number(localeString.split(":").join(""))
+            );
+            dateObjectList.dateTimes.end.push(
+              Number(localeStringEnd.split(":").join(""))
+            );
+          }
+        });
+        return setDateLists((prevState) => ({ ...prevState, dateObjectList }));
+      })
+      .catch((err) => console.log(err));
   };
   useEffect(() => {
     dateFormatting();
-  });
+  }, []);
 
   const disabledDates = (date) => {
     let day = new Date(date["$d"]).getDay();
