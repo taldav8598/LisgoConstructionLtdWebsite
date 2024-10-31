831c84894931ba5f6c62712fb09fbdbea332a38d
Cascade8598 <tmdft360@gmail.com>
2024-10-31 15:41:15 +0000

    fix: to responsiveness of contact us containers

--- client/src/ContactUs.jsx
+++ client/src/ContactUs.jsx
@@ -1,31 +1,32 @@
-import { Link, Typography } from "@mui/material";
+import { Grid, Link, Typography } from "@mui/material";
 import EmailIcon from "@mui/icons-material/Email";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import "./ContactUs.css";
+import Grid2 from "@mui/material/Unstable_Grid2";
 
 export default function ContactUs() {
   return (
     <section id="contact-us" className="contact-us-section">
       <div className="contact-us-container">
         <Typography variant="h3">Contact Us</Typography>
-        <div className="contact-us-grid">
-          <div className="contact-item-container">
+        <Grid container className="contact-us-grid">
+          <Grid size={10} className="contact-us-item-container">
             <div className="email-container">
               <EmailIcon className="email-icon" />
             </div>
             <Typography variant="h4">Email</Typography>
             <Typography variant="body">lisgoconstruction@gmail.com</Typography>
-          </div>
-          <div className="contact-item-container">
+          </Grid>
+          <Grid size={10} className="contact-us-item-container">
             <div className="phone-no-container">
               <LocalPhoneIcon className="phone-icon" />
             </div>
             <Typography variant="h4">Phone</Typography>
             <Typography variant="body">07732 107825</Typography>
-          </div>
-          <div className="contact-item-container">
+          </Grid>
+          <Grid size={10} className="contact-us-item-container">
             <div className="facebook-container">
               <FacebookIcon className="facebook-icon" />
             </div>
@@ -36,17 +37,17 @@ export default function ContactUs() {
             >
               follow us
             </Link>
-          </div>
-          <div className="contact-item-container">
+          </Grid>
+          <Grid size={10} className="contact-us-item-container">
             <div className="location-on-container">
               <LocationOnIcon className="location-on-icon" />
             </div>
             <Typography variant="h4">Location</Typography>
-            <Typography variant="body" class="address">
+            <Typography variant="body" className="address">
               24 woodstock road, Manchester, United Kingdom
             </Typography>
-          </div>
-        </div>
+          </Grid>
+        </Grid>
       </div>
     </section>
   );
