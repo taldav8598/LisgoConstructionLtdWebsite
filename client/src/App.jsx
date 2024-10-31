9e708aa368d80e47b9c5b199c94fce5c8c6d5905
Cascade8598 <tmdft360@gmail.com>
2024-10-31 09:02:27 +0000

    feature: adds contact us page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,30 @@
-import { Container } from '@mui/material';
+import { Container } from "@mui/material";
 
-import Navigation from './Navigation';
-import Home from  './Home';
-import EnquireNow from  './EnquireNow';
-import Footer from './Footer'
-import AboutUs from './AboutUs';
-import Services from './Services';
-import Gallery from './Gallery';
-import './App.css';
+import Navigation from "./Navigation";
+import Home from "./Home";
+import EnquireNow from "./EnquireNow";
+import Footer from "./Footer";
+import AboutUs from "./AboutUs";
+import Services from "./Services";
+import Gallery from "./Gallery";
+import ContactUs from "./ContactUs";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-       <Navigation/>
-    <Container className='app-container' maxWidth="lg">
-      <Home/>
-      <EnquireNow/>
-      <Services />
-      <div id="aboutUs">
-      <AboutUs />
-      </div>
-      <Gallery />
-    </Container>
-    <Footer />
+      <Navigation />
+      <Container className="app-container" maxWidth="lg">
+        <Home />
+        <EnquireNow />
+        <Services />
+        <div id="aboutUs">
+          <AboutUs />
+        </div>
+        <Gallery />
+        <ContactUs />
+      </Container>
+      <Footer />
     </>
   );
 }
---
+++ client/src/ContactUs.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export default function ContactUs() {
+  return (
+    <section id="contact-us" className="contact-us-section">
+      <div className="contact-us-container">
+        <Typography variant="h3">Contact Us</Typography>
+      </div>
+    </section>
+  );
+}
