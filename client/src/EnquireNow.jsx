573a3ce2bf4c795b9c37a82058a617fe263e224e
Cascade8598 <tmdft360@gmail.com>
2024-11-11 13:09:21 +0000

    fix: moved disbledTimes and disableDates util functions to utils folder

--- client/src/EnquireNow.jsx
+++ client/src/EnquireNow.jsx
@@ -19,6 +19,8 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 import { useEffect, useState } from "react";
 import { dateFormatting } from "./utils/dateFormatting";
+import { disabledDates } from "./utils/disabledDates";
+import { disabledTimes } from "./utils/disabledTimes";
 import emailjs from "emailjs-com";
 // import axios from "axios";
 import "./EnquireNow.css";
@@ -103,124 +105,6 @@ export default function EnquireNow() {
   useEffect(() => {
     dateFormatting(apiUrl, setDateLists);
   }, []);
-
-  const disabledDates = (date) => {
-    let day = new Date(date["$d"]).getDay();
-    const formattedDate = `${date["$y"]}${
-      date["$M"] + 1 < 10 ? "0" + (date["$M"] + 1) : date["$M"] + 1
-    }${date["$D"] < 10 ? "0" + date["$D"] : date["$D"]}`;
-
-    let numberDate = Number(formattedDate.split("-").join(""));
-
-    if (days[day] === "Saturday") {
-      return true;
-    } else if (days[day] === "Sunday") {
-      const indicesArr = dateLists.dateObjectList?.dates
-        .map((date, index) => {
-          if (date === Number(formattedDate)) {
-            return index;
-          }
-        })
-        .filter((element) => element !== undefined);
-
-      const selectedDateTimes = {
-        9: [],
-        10: [],
-        11: [],
-        12: [],
-        13: [],
-        14: [],
-        15: [],
-        16: [],
-      };
-
-      for (let i = 0; i < indicesArr?.length; i++) {
-        const timeStr = String(dateLists.dateObjectList.dateTimes.start[i]);
-        const minutes = Number(timeStr.at(-4) + timeStr.at(-3));
-
-        selectedDateTimes[
-          (
-            dateLists.dateObjectList.dateTimes.start[indicesArr[i]] / 10000
-          ).toFixed()
-        ].push(minutes);
-      }
-
-      const allTimesBooked = Object.values(selectedDateTimes).every(
-        (arr) => arr.length === 2
-      );
-
-      if (allTimesBooked) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return dateLists.dateObjectList?.dates.includes(numberDate);
-    }
-  };
-
-  const disabledTimes = (date) => {
-    let day = new Date(date["$d"]).getDay();
-    let hour = date["$H"];
-
-    const formattedDate = `${date["$y"]}${
-      date["$M"] + 1 < 10 ? "0" + (date["$M"] + 1) : date["$M"] + 1
-    }${date["$D"] < 10 ? "0" + date["$D"] : date["$D"]}`;
-
-    if (
-      days[day] === "Sunday" &&
-      !dateLists.dateObjectList?.dates.includes(Number(formattedDate))
-    ) {
-      if (!(hour >= 9 && hour <= 16)) {
-        return true;
-      }
-    } else if (
-      days[day] === "Sunday" &&
-      dateLists.dateObjectList?.dates.includes(Number(formattedDate))
-    ) {
-      let minute = date["$m"];
-
-      const indicesArr = dateLists.dateObjectList?.dates
-        .map((date, index) => {
-          if (date === Number(formattedDate)) {
-            return index;
-          }
-        })
-        .filter((element) => element !== undefined);
-
-      const selectedDateTimes = {
-        9: [],
-        10: [],
-        11: [],
-        12: [],
-        13: [],
-        14: [],
-        15: [],
-        16: [],
-      };
-
-      for (let i = 0; i < indicesArr?.length; i++) {
-        const timeStr = String(dateLists.dateObjectList.dateTimes.start[i]);
-        const minutes = Number(timeStr.at(-4) + timeStr.at(-3));
-
-        selectedDateTimes[
-          (
-            dateLists.dateObjectList.dateTimes.start[indicesArr[i]] / 10000
-          ).toFixed()
-        ].push(minutes);
-      }
-
-      if (!(hour >= 9 && hour <= 16)) {
-        return true;
-      } else if (selectedDateTimes[hour]?.includes(minute)) {
-        return true;
-      } else if (selectedDateTimes[hour].length === 2) {
-        return true;
-      }
-    } else {
-      return false;
-    }
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -414,8 +298,12 @@ export default function EnquireNow() {
                     }}
                     className="date-time-picker"
                     defaultValue={today}
-                    shouldDisableDate={disabledDates}
-                    shouldDisableTime={disabledTimes}
+                    shouldDisableDate={(date) =>
+                      disabledDates(date, dateLists, days)
+                    }
+                    shouldDisableTime={(date) =>
+                      disabledTimes(date, dateLists, days)
+                    }
                     format={
                       days[new Date(dateInput).getDay()] === "Sunday"
                         ? "DD/MM/YYYY hh:mm"
