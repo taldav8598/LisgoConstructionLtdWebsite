32c014eb70be1acc9b5b558f75908fc974a04c7b
Cascade8598 <tmdft360@gmail.com>
2024-11-15 08:16:49 +0000

    fix: to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  base: "/LisgoConstructionLtdWebsite",
   define: {
     "process.env": {},
   },
